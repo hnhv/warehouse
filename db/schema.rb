37dbca312bdedf4af788f42926b675710cd0faec
Heinrich Vogel <heinrichvogel@Heinrichs-MacBook-Pro-2.local>
2021-01-16 23:36:23 +0200

    add model associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_152433) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "name"
-    t.integer "quantity"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_210_116_152_433) do
+  create_table 'articles', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'quantity'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "articles_products", id: false, force: :cascade do |t|
-    t.integer "article_id", null: false
-    t.integer "product_id", null: false
+  create_table 'articles_products', id: false, force: :cascade do |t|
+    t.integer 'article_id', null: false
+    t.integer 'product_id', null: false
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.decimal "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'products', force: :cascade do |t|
+    t.string 'name'
+    t.decimal 'price'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
