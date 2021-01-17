8499b8bdc13522d45b59ffbdafb0520e0447a023
Heinrich Vogel <heinvogel@gmail.com>
2021-01-17 15:06:44 +0200

    create get /products and get /products/:id routes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,10 @@ class Product < ApplicationRecord
   has_many :product_articles
   has_many :articles, through: :product_articles
   validates_presence_of :name, :price
+
+  def quantity_available
+    self.articles.each do |article|
+      # 
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,6 +22,7 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -81,4 +82,5 @@ RSpec.configure do |config|
       example.run
     end
   end
+  config.include RequestSpecHelper, type: :request
 end
